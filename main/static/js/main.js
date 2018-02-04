ab008e8379208b197d18549a2c7ce4076b169e03
bsnacks000 <bsnacks000@gmail.com>
2018-02-03 20:00:41 -0500

    added jquery to get filename from text box

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,1 +1,8 @@
-// main.js placeholder 
+// main.js placeholder
+$(document).ready(function(){
+
+    $('#id_upload_image').on('change', (e)=>{
+        let fname = e.target.files[0].name
+        $('#filename').text(fname);
+    });
+});
